006ed58b6c4501b032e4e0cce8326daef03ecc88
wdclub <60172551+wdclub@users.noreply.github.com>
2022-03-05 20:02:27 +0000

    Update get-node.sh

--- containers/etho/get-node.sh
+++ containers/etho/get-node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GIT_INFO=$(curl -sL "https://api.github.com/repos/Ether1Project/Ether-1-SN-MN-Binaries/releases/latest")                                       
+GIT_INFO=$(curl -sL "https://api.github.com/repos/Ether1Project/Ether1/releases/latest")                                       
 URL=$(printf "%s" "$GIT_INFO" | jq .assets[].browser_download_url -r)                        
 
 for row in $URL; do 
@@ -13,8 +13,8 @@ if [ -f "./limits.conf" ]; then
     if grep "NODE_VERSION=" "./limits.conf"; then 
         NODE_BINARY=$(grep "NODE_VERSION=" "./limits.conf" | sed 's/NODE_VERSION=//g')
        
-        if curl --fail -sL "https://api.github.com/repos/Ether1Project/Ether-1-SN-MN-Binaries/releases/tags/$NODE_BINARY"; then
-            GIT_INFO=$(curl -sL "https://api.github.com/repos/Ether1Project/Ether-1-SN-MN-Binaries/releases/tags/$NODE_BINARY")                                       
+        if curl --fail -sL "https://api.github.com/repos/Ether1Project/Ether1/releases/tags/$NODE_BINARY"; then
+            GIT_INFO=$(curl -sL "https://api.github.com/repos/Ether1Project/Ether1/releases/tags/$NODE_BINARY")                                       
             URL=$(printf "%s\n" "$GIT_INFO" | jq .assets[].browser_download_url -r)      
             for row in $URL; do 
                 if basename "$row" | grep Ether1 > /dev/null; then 
@@ -47,3 +47,4 @@ cp -f "$(find . -name geth)" . 2>/dev/null
 printf "%s" "$(printf "%s" "$GIT_INFO" | jq .tag_name -r | sed 's\v\\')" > ./version
 
 exit 0
+
